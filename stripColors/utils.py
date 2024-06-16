a823f4fedb0c8f2338fb94606fa1a00f59c8d156
Subru-37 <mdl21ec119@mec.ac.in>
2024-06-16 11:59:19 +0530

    made the code cleaner

---
+++ stripColors/utils.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+
+def stripColors(img):
+    num_squares = 10
+
+    height, width, _ = img.shape                                                 # Calculate the width of each square
+    img = cv2.resize(img, (int(width/1.8),int( height/1.8)))                     # resize the image for better viewing 
+    image = img[int(height*0.01):int(height*0.37),int(width*0.3):int(width*0.4)] # Crop the image for better accuracy while diving 
+    square_height = image.shape[0] // num_squares                                # image into 10 sections along with height
+    
+    data = []
+    for i in range(num_squares):
+        y1 = int(i * square_height)                                              # Define the coordinates of the squares in each iteration
+        y2 = int((i + 1) * square_height)
+        x1 = 0
+        x2 = width
+
+        b,g,r = cv2.split(image[y1:y2, x1:x2])
+        data.append([np.mean(r), np.mean(g), np.mean(b)])
+        
+    my_dict = {
+        'URO': data[0],
+        'BIL': data[1],
+        'KET': data[2],
+        'BLD': data[3],
+        'PRO': data[4],
+        'NIT': data[5],
+        'LEU': data[6],
+        'GLU': data[7],
+        'SG': data[8],
+        'PH': data[9]
+    }
+    
+    return my_dict
--- stripColors/views.py
+++ stripColors/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 import cv2
 import numpy as np
-# Create your views here.
 from django.http import HttpResponse
 from django.http import Http404
-# from django.http import Http
-
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpRequest
 from .models import ImageModel
-from .myfunctions import stripsColors
-import json
+from .utils import stripColors
 
 
 def index(request):
@@ -20,26 +16,18 @@ def index(request):
 def processImage(request):
     if request.method == 'POST':
         id = 0
-        # print(id)
+
         try:
             id = request.POST.get('id')
-            
-            # Access uploaded file(s)
-            if 'image' in request.FILES:
+            if 'image' in request.FILES:                                # Access uploaded file(s)
                 image_file = request.FILES['image']
         except:
             raise ValueError("request body not loaded")
-        # print(id)
+
         if id:
-            # request_data = json.loads(request.body)
-            # # id = request_data.get('id')  # Extract ID from request body
-            # image_file = file_data.image
-            # id = request_data.id
+
             img = cv2.imdecode(np.fromstring(image_file.read(), np.uint8), cv2.IMREAD_COLOR)
-            # processed_image = ImageModel(image=image_file, name=id)
-            # processed_image.save()
-            data = stripsColors(img)
+            data = stripColors(img)
+
             return HttpResponse(JsonResponse(data))
-        
-        # converting image into grayscale image 
         
